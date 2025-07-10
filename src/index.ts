353cee0f9d396cd1feafe6b3356922220c3272b4
Farsana kt <ffarsanakt@gmail.com>
2025-07-10 19:20:22 +0530

    updated cors

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        origin:"http://localhost:8080",
+        origin:"https://api.ibookingvenue.com",
         credentials:true
     })
 )
